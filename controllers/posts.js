f57f2aea6184ed5fb518149918e7bc6a8b40b6b3
Jukem <chris@dixn.dev>
2023-04-17 13:19:52 -0700

    removed unused game page

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,7 +4,7 @@ const Post = require("../models/Post");
 module.exports = {
   getProfile: async (req, res) => {
     try {
-      const posts = await Post.find({ assigned: req.user.id });
+      const posts = await Post.find({ assigned: req.user.color });
       res.render("profile.ejs", { posts: posts, user: req.user });
     } catch (err) {
       console.log(err);
